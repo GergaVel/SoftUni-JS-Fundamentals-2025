baa6f75a56e20d1919aac07ad626760edb7bec05
GergaVel <gergana.velikova3@gmail.com>
2025-09-22 12:05:58 +0200

    update

--- Data types and Variables - Excersice/07.lowerOrUpper.js
+++ Data types and Variables - Excersice/07.lowerOrUpper.js
@@ -1,7 +1,7 @@
 function checkCase (char){
     if(char === char.toUpperCase()){
         console.log('upper-case');        
-    } else if (char === char.toLowerCase()){
+    } else {
         console.log('lower-case');        
     }
 
