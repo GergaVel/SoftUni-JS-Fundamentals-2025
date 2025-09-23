5d575cbf83066f4b9e14beb1e6efc199e8c419ef
GergaVel <gergana.velikova3@gmail.com>
2025-09-23 20:06:24 +0200

    update

--- Data types and Variables - Excersice/10.spiceMustFlow.js
+++ Data types and Variables - Excersice/10.spiceMustFlow.js
@@ -1,5 +1,26 @@
-function spice(start){
+function spice(sourceYield){
+    let days = 0;
+    let spiceExtracted = 0;
 
+    while (sourceYield >= 100){
+        days++;
+        spiceExtracted += sourceYield;
+        sourceYield -= 10;
+
+        if(spiceExtracted >= 26) {
+            spiceExtracted -= 26;
+        }
+
+    }
+
+    if(spiceExtracted >= 26) {
+        spiceExtracted -= 26;
+    }
+
+    console.log(days);
+    console.log(spiceExtracted);
+    
+    
 }
 
 spice(111);
