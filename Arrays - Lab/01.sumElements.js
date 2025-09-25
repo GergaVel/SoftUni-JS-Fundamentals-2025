9ba451c90bc727630b3600d1dcb52f9fd0864e4f
GergaVel <gergana.velikova3@gmail.com>
2025-09-25 21:51:37 +0200

    Tasks from new lesson

---
+++ Arrays - Lab/01.sumElements.js
@@ -0,0 +1,9 @@
+function sumElements(arr){
+    let firstElement = arr[0];
+    let lastElement = arr[arr.length - 1];
+
+    console.log(firstElement + lastElement);
+
+}
+
+sumElements([20, 30, 40]);
