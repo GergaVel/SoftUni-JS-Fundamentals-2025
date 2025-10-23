c7b3a9c649a6696f6d518eb928cf40cd87d5c23b
GergaVel <gergana.velikova3@gmail.com>
2025-10-23 14:13:13 +0200

    Tasks from new lesson

--- Objects and Classes/06.songs.js
+++ Objects and Classes/06.songs.js
@@ -1,5 +1,39 @@
-function songs(data   ){
+function songs(songsDataArr){
+    class Songs {
+        constructor(typeList, name, time){
+            this.typeList = typeList;
+            this.name = name;
+            this.time = time;
+        }
+    }
 
+    let numberOfSongs = songsDataArr[0];
+
+    let allSongs = [];
+
+    for (let i = 1; i <= numberOfSongs; i++){
+        let parsedData = songsDataArr[i].split('_');
+        
+        let type = parsedData[0];
+        let songName = parsedData[1];
+        let songTime = parsedData[2];
+
+        let song = new Songs (type, songName, songTime); //I create a song
+
+        allSongs.push(song); // I add it to the []
+    }
+        let searchTerm = songsDataArr.pop(); //to extract the last el
+
+        if (searchTerm !== 'all'){
+            allSongs = allSongs
+                .filter(s => s.typeList === searchTerm);
+        }
+
+        let songNames = allSongs.map(s => s.name);
+
+        for (let songName of songNames) { //to print only part of the data
+            console.log(songName);             
+        }
 }
 
 songs([3,
