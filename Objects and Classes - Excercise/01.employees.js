adfdc3fcfea9bf8e4cf6637bd09277b5e156d591
GergaVel <gergana.velikova3@gmail.com>
2025-10-24 21:53:00 +0200

    Tasks from new lesson

---
+++ Objects and Classes - Excercise/01.employees.js
@@ -0,0 +1,56 @@
+// 1st version
+function employees(arr) {
+    let result = []; //will keep objs
+
+    for (let employee of arr){
+        let employeeObj = {
+            name: employee,
+            personalNumber: employee.length
+        };
+
+        result.push(employeeObj);
+    } 
+
+   // console.log(result[0].name); // you can acess any type of info
+    result.forEach((em) => {
+        console.log(`Name: ${em.name} -- Personal Number: ${em.personalNumber}`);
+        
+    })
+}
+
+employees([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal']);
+
+//2nd version
+function employees(arr) {
+    class Employee {
+        constructor (name, personalNumber){
+            this.name = name;
+            this.personalNumber = personalNumber;
+        }
+
+        data() {
+            return `Name: ${this.name} -- Personal Number: ${this.personalNumber}`
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++){
+        let command = arr[i];
+        let names = command;
+        let namesLength = names.length;
+
+        let employeeList = new Employee(names, namesLength);
+            console.log(employeeList.data());            
+        }
+    }
+
+
+employees([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal']);
+
