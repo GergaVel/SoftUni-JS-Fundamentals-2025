ed80ca533e4ddeae9fb7c84103d6c664b3d95619
GergaVel <gergana.velikova3@gmail.com>
2025-10-27 18:28:00 +0100

    Tasks from new lesson

--- Objects and Classes - Excercise/09.catalogue.js
+++ Objects and Classes - Excercise/09.catalogue.js
@@ -24,3 +24,14 @@ function storeCatalog(productsArr) {
     console.log(`  ${product.name}: ${product.price}`);
   }
 }
+
+storeCatalog([
+    'Appricot : 20.4',
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15',
+    'T-Shirt : 10'
+]);
