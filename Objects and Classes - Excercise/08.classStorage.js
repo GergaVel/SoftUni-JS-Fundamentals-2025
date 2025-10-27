e01932190e7fde345e86e9ceb750004422165b72
GergaVel <gergana.velikova3@gmail.com>
2025-10-27 17:40:35 +0100

    Tasks from new lesson

--- Objects and Classes - Excercise/08.classStorage.js
+++ Objects and Classes - Excercise/08.classStorage.js
@@ -1,3 +1,28 @@
+class Storage {
+    constructor (capacity) {
+        this.capacity = capacity;
+        this.storage = []; //will be filled up later
+        this.totalCost = 0; //because we haven't bougth anything
+    }
+
+    addProduct(productObj){
+        this.storage.push(productObj); //to push it in the empty []
+        this.capacity -= productObj.quantity;
+        this.totalCost += productObj.price * productObj.quantity;
+    }
+
+    getProducts(){
+        let productJsons = [];
+
+        for (let productObj of this.storage){
+           productJsons.push(JSON.stringify(productObj)); //we can't use return here, because the loop will stop after the first round
+        }
+
+        return productJsons.join('\n');
+    }
+}
+
+
 let productOne = {name: 'Cucamber', price: 1.50, quantity: 15};
 let productTwo = {name: 'Tomato', price: 0.90, quantity: 25};
 let productThree = {name: 'Bread', price: 1.10, quantity: 8};
