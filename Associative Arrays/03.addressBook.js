d7b54697639998759c82ece4aaaf6e33d14f0f74
GergaVel <gergana.velikova3@gmail.com>
2025-10-28 22:59:44 +0100

    Tasks from new lesson

---
+++ Associative Arrays/03.addressBook.js
@@ -0,0 +1,38 @@
+function solve(data) {
+    let addressBook = {};
+
+    for (let entry of data){
+        let parsedData = entry.split(':');
+
+        [firstName, address] = parsedData;
+
+        addressBook[firstName] = address; //no if-else because we are re-writing anyways
+    }
+
+    let addressBookEntries = Object.entries(addressBook); //we get array of arrays
+   
+    addressBookEntries.sort((first, second) => {
+        let firstKey = first[0];
+        let secondKey = second [0];
+
+        return firstKey.localeCompare(secondKey);
+    });
+
+    for (let [key, value] of addressBookEntries){
+        console.log(`${key} -> ${value}`);
+        
+    }    
+}
+
+solve(['Bob:Huxley Rd',
+    'John:Milwaukee Crossing',
+    'Peter:Fordem Ave',
+    'Bob:Redwing Ave',
+    'George:Mesta Crossing',
+    'Ted:Gateway Way',
+    'Bill:Gateway Way',
+    'John:Grover Rd',
+    'Peter:Huxley Rd',
+    'Jeff:Gateway Way',
+    'Jeff:Huxley Rd'
+]);
