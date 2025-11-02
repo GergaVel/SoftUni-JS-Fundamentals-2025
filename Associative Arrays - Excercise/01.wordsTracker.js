106e137e3e34ecb605e1dbe56139574ae768f2fc
GergaVel <gergana.velikova3@gmail.com>
2025-11-02 21:58:47 +0100

    Tasks from new lesson

---
+++ Associative Arrays - Excercise/01.wordsTracker.js
@@ -0,0 +1,30 @@
+function wordsTracker(arr) {
+    let wordOccurences = {};
+
+    let wordsToTrack = arr.shift().split(' ');
+   
+    for (let word of wordsToTrack){
+        wordOccurences[word] = 0; //i get into the key and add value
+    } 
+
+    for (let word of arr){
+        if (word in wordOccurences){
+            wordOccurences[word]++;
+        }  //no else because we don't need the info
+    }
+
+    let sorted = Object.entries(wordOccurences).sort((a, b) => b[1] - a[1]);
+    
+    for(let [key, value] of sorted){
+        console.log(`${key} - ${value}`);        
+    }
+    
+}
+
+wordsTracker([
+    'this sentence',
+    'In', 'this', 'sentence', 'you', 'have',
+    'to', 'count', 'the', 'occurrences', 'of',
+    'the', 'words', 'this', 'and', 'sentence',
+    'because', 'this', 'is', 'your', 'task'
+]);
