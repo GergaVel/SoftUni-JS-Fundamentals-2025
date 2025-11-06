73458898824e2bd2e407c33acaa94e6710b14dff
GergaVel <gergana.velikova3@gmail.com>
2025-11-06 12:34:04 +0100

    Tasks from new lesson

--- Text processing - Excercise/01.revealWords.js
+++ Text processing - Excercise/01.revealWords.js
@@ -1,5 +1,14 @@
-function revealWords(){
+function revealWords(wordsStr, text){
+   let wordsToFill = wordsStr.split(', ');
 
+        for (let word of wordsToFill){
+            let starsCount = '*'.repeat(word.length);
+            text = text.replace(starsCount, word);
+        }
+               
+    console.log(text);        
 }
 
-revealWords('great', 'softuni is ***** place for learning new programming languages');
+revealWords('great', 
+    'softuni is ***** place for learning new programming languages'
+);
