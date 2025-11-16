71edc2d0ac1d090a05f60a65152acd5405fe1fc9
GergaVel <gergana.velikova3@gmail.com>
2025-11-16 15:53:49 +0100

    Tasks from new lesson

--- Objects and Classes - Excercise/06.makeADictionary.js
+++ Objects and Classes - Excercise/06.makeADictionary.js
@@ -1,8 +1,18 @@
-//Term: Boiler => Definition: A fuel-burning apparatus or container for heating water.
-
 function dictionary (arr){
-    
+    dictionary = {};
 
+    for (let jsonStr of arr){
+        let obj = JSON.parse(jsonStr); // convert it into an object
+        let entries = Object.entries(obj); // returns and array: [['Coffee', 'A hot drink made\n...']]
+        let [term, definition] = entries[0]; //important to rember this [0] - I didn't fully get it but still
+       
+        dictionary[term] = definition;                
+    }
+     let sorted = Object.entries(dictionary).sort((a,b) => a[0].localeCompare(b[0]));
+
+        for (let [term, definition] of sorted){
+            console.log(`Term: ${term} => Definition: ${definition}`);            
+        }
 }
 
 dictionary([
@@ -12,3 +22,4 @@ dictionary([
 '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
 '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified,transmitted, or recorded."}'
 ]);
+
