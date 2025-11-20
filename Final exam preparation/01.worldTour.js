dd8e8cf96ebdf0403a4e61b983044485ac2d214f
GergaVel <gergana.velikova3@gmail.com>
2025-11-20 08:47:37 +0100

    Final exam preparaton

--- Final exam preparation/01.worldTour.js
+++ Final exam preparation/01.worldTour.js
@@ -1,5 +1,115 @@
 function worldTour(input){
+    let destinations = input.shift(); //initial string that we will manipulate
+    
+    for (let line of input){
+        let [command, ...params] = line.split(':');
+        
+        if (command === 'Travel'){
+            console.log(`Ready for world tour! Planned stops: ${destinations}`);
+                break;
+        }
 
+        if (command === 'Add Stop'){
+            let index = Number(params[0]);
+            let string = params[1];
+
+                if (index >= 0 && index < destinations.length){
+                    destinations = destinations.substring(0, index) +
+                                   string +
+                                   destinations.substring(index);
+
+                    console.log(destinations);                    
+                }
+
+        } else if (command === 'Remove Stop'){
+            let startIdx = Number(params[0]);
+            let endIdx = Number(params[1]);
+
+             if ((startIdx >= 0 && startIdx < destinations.length) && 
+                 (endIdx >= 0 && endIdx < destinations.length)){
+                    
+                    destinations = destinations.substring(0, startIdx) +
+                                   destinations.substring(endIdx + 1); // from 0 - startIdx and then start form end Idx till end (middle part goes away)
+                   
+                    console.log(destinations);                  
+                }
+
+        } else if (command === 'Switch'){
+            let oldString = params[0];
+            let newString = params[1];
+
+            while (destinations.includes(oldString)){
+                destinations = destinations.replace(oldString, newString);
+            }
+
+            console.log(destinations);       
+        }
+    } 
 }
 
-worldTour(['Hawai::Cyprys-Greece Add Stop:7:Rome Remove Stop:11:16 Switch:Hawai:Bulgaria Travel']);
+worldTour(['Hawai::Cyprys-Greece', 
+    'Add Stop:7:Rome', 
+    'Remove Stop:11:16',
+    'Switch:Hawai:Bulgaria',
+    'Travel'
+]);
+
+
+// version with a side function to help check if an eindex is valid
+function worldTour(input) {
+    let destinations = input.shift();
+
+    const isValid = (i) => i >= 0 && i < destinations.length;
+
+    for (let line of input) {
+        let [command, ...params] = line.split(':');
+
+        if (command === 'Travel') {
+            console.log(`Ready for world tour! Planned stops: ${destinations}`);
+            break;
+        } 
+        
+        if (command === 'Add Stop') {
+            let index = Number(params[0]);
+            let str = params[1];
+
+            if (isValid(index)) {
+                destinations =
+                    destinations.slice(0, index) +
+                    str +
+                    destinations.slice(index);
+            }
+
+            console.log(destinations);
+        } 
+        
+        else if (command === 'Remove Stop') {
+            let start = Number(params[0]);
+            let end = Number(params[1]);
+
+            if (isValid(start) && isValid(end)) {
+                destinations =
+                    destinations.slice(0, start) +
+                    destinations.slice(end + 1);
+            }
+
+            console.log(destinations);
+        } 
+        
+        else if (command === 'Switch') {
+            let oldStr = params[0];
+            let newStr = params[1];
+
+            destinations = destinations.split(oldStr).join(newStr);
+
+            console.log(destinations);
+        }
+    }
+}
+
+worldTour(['Hawai::Cyprys-Greece', 
+    'Add Stop:7:Rome', 
+    'Remove Stop:11:16',
+    'Switch:Hawai:Bulgaria',
+    'Travel'
+]);
